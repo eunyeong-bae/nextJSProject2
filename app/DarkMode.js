8bbd79c6b264ae09c11b656d030c1f1fb0872970
배은영 <adcadcad@naver.com>
2023-11-01 17:45:30 +0900

    다크모드 재수정

--- app/DarkMode.js
+++ app/DarkMode.js
@@ -8,6 +8,7 @@ export default function DarkMode({cookie}) {
     
     useEffect(() => {
         if(cookie == undefined){
+            console.log("3st",cookie)
             //mode 라는 이름의 쿠키가 없으면 실행해주세요 조건 추가 필요
             document.cookie = 'mode=light; max-age=' + (3600 * 24 * 400);
         } 
@@ -16,9 +17,9 @@ export default function DarkMode({cookie}) {
 
     return (
         <span onClick={() => { 
-            document.cookie = cookie != undefined && cookie === 'dark'? "mode=light; max-age="+(3600 * 24 * 400) : "mode=dark; max-age="+(3600 * 24 * 400)
+            document.cookie = cookie.value === 'dark'? "mode=light; max-age="+(3600 * 24 * 400) : "mode=dark; max-age="+(3600 * 24 * 400)
             router.refresh()            
         }}
-        >{cookie != undefined && cookie == 'dark' ? '☀️' : '🌙'}</span>
+        >{cookie && cookie.value == 'dark' ? '☀️' : '🌙'}</span>
     )
 }
