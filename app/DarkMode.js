3d27087d27ee89dff2eb2114d017f547fb3100ed
배은영 <adcadcad@naver.com>
2023-11-01 17:53:16 +0900

    다크모드 재수정3

--- app/DarkMode.js
+++ app/DarkMode.js
@@ -1,25 +1,27 @@
 'use client'
 
 import { useRouter } from "next/navigation";
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function DarkMode({cookie}) {
     let router = useRouter();
-    
+    const [cookieValue, setCookieValue] = useState(cookie.value);
+    console.log('test: ',cookieValue)
+
     useEffect(() => {
-        if(cookie == undefined){
-            console.log("3st",cookie)
+        if(cookieValue == undefined){
             //mode 라는 이름의 쿠키가 없으면 실행해주세요 조건 추가 필요
             document.cookie = 'mode=light; max-age=' + (3600 * 24 * 400);
+            setCookieValue('light');
         } 
 
     }, [])
 
     return (
         <span onClick={() => { 
-            document.cookie = cookie.value === 'dark'? "mode=light; max-age="+(3600 * 24 * 400) : "mode=dark; max-age="+(3600 * 24 * 400)
+            document.cookie = cookieValue === 'dark'? "mode=light; max-age="+(3600 * 24 * 400) : "mode=dark; max-age="+(3600 * 24 * 400)
             router.refresh()            
         }}
-        >{cookie && cookie.value == 'dark' ? '☀️' : '🌙'}</span>
+        >{cookieValue != undefined && cookieValue == 'dark' ? '☀️' : '🌙'}</span>
     )
 }
